55b9477cb8b6a3b1c07ea8ddd287cf2ca9ad5642
tlarbals824 <simkm0824@naver.com>
2024-01-18 20:43:27 +0900

    Initial commit

---
+++ src/main/java/com/backend/makemyimage/MakeMyImageApplication.java
@@ -0,0 +1,13 @@
+package com.backend.makemyimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMyImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeMyImageApplication.class, args);
+    }
+
+}
