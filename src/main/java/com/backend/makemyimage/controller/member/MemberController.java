8e7b88f53bebf98d78a1735dbf9c21a312af02a1
재국 <jk6722@hanyang.ac.kr>
2024-01-28 00:16:42 +0900

    feat: 회원 컨트롤러 생성

---
+++ src/main/java/com/backend/makemyimage/controller/member/MemberController.java
@@ -0,0 +1,33 @@
+package com.backend.makemyimage.controller.member;
+
+import com.backend.makemyimage.base.BaseResponse;
+import com.backend.makemyimage.dto.request.member.LoginRequest;
+import com.backend.makemyimage.dto.response.member.LoginResponse;
+import com.backend.makemyimage.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class MemberController {
+    private final MemberService memberService;
+
+    //로그인
+    @PostMapping("/login")
+    public BaseResponse<LoginResponse> memberLogin(@RequestBody LoginRequest req, BindingResult bindingResult) throws Exception {
+        if(bindingResult.hasErrors()) {
+            //에러 발생
+            ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
+            return BaseResponse.onFailure(400, objectError.getDefaultMessage(), null);
+        }
+
+        LoginResponse response = memberService.userLogin(req);
+        return BaseResponse.onSuccess(response);
+    }
+}
