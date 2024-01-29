f895f361d1783041088b687db4589bcf090fafc1
재국 <jk6722@hanyang.ac.kr>
2024-01-29 16:32:15 +0900

    feat: 이미지 삭제 메서드 추가

--- src/main/java/com/backend/makemyimage/repository/image/ImageRepository.java
+++ src/main/java/com/backend/makemyimage/repository/image/ImageRepository.java
@@ -1,4 +1,4 @@
-package com.backend.makemyimage.repository;
+package com.backend.makemyimage.repository.image;
 
 import com.backend.makemyimage.domain.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface ImageRepository extends JpaRepository<Image, Long> {
+public interface ImageRepository extends JpaRepository<Image, Long>, ImageRepositoryCustom {
     Optional<Image> findById(Long id);
     List<Image> findAllByLoginId(String loginId);
 }
