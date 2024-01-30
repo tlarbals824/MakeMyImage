86750261aca905099b27920639cecc27ea1a6749
재국 <jk6722@hanyang.ac.kr>
2024-01-30 16:14:47 +0900

    feat: 로그인 응답 dto에 유저정보 추가

--- src/main/java/com/backend/makemyimage/dto/response/member/LoginResponse.java
+++ src/main/java/com/backend/makemyimage/dto/response/member/LoginResponse.java
@@ -7,10 +7,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class LoginResponse {
-
+    private String loginId;
+    private String email;
     private String accessToken;
+
     @Builder
-    public LoginResponse(String accessToken) {
+    public LoginResponse(String loginId, String email, String accessToken) {
+        this.loginId = loginId;
         this.accessToken = accessToken;
+        this.email = email;
     }
 }
