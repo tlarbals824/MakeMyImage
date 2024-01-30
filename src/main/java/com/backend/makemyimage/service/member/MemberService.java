cbf250f5d28f8eb4615c1d6e7f9821b5805f2231
재국 <jk6722@hanyang.ac.kr>
2024-01-30 16:16:33 +0900

    refactor: 사용하지 않는 변수 주석처리

--- src/main/java/com/backend/makemyimage/service/member/MemberService.java
+++ src/main/java/com/backend/makemyimage/service/member/MemberService.java
@@ -5,7 +5,7 @@ import com.backend.makemyimage.dto.request.member.LoginRequest;
 import com.backend.makemyimage.dto.request.member.MemberRequest;
 import com.backend.makemyimage.dto.response.member.LoginResponse;
 import com.backend.makemyimage.dto.response.member.MemberInfoResponse;
-import com.backend.makemyimage.repository.MemberRepository;
+import com.backend.makemyimage.repository.member.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,33 +33,37 @@ public class MemberService {
     }
 
     @Transactional
-    public LoginResponse userLogin(LoginRequest req) throws RuntimeException {
+    public LoginResponse userLogin(LoginRequest req) {
         //유저 로그인
         Member findMember = memberRepository.findByLoginId(req.getLoginId())
-                .orElseThrow(() -> new RuntimeException("잘못된 아이디 혹은 비밀번호 입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 아이디 혹은 비밀번호 입니다."));
 
         if(!findMember.getPassword().equals(req.getPassword())) {
             // 비밀번호가 일치하지 않는다면
-            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
-        String secretKey = "make-my-image";
-        long expireTime = 1000 * 60 * 60; // 토큰 만료시간 = 60분
+//        String secretKey = "make-my-image";
+//        long expireTime = 1000 * 60 * 60; // 토큰 만료시간 = 60분
         
         // Jwt accessToken 생성
         String accessToken = "access-token";
-        return new LoginResponse(accessToken);
+        return LoginResponse.builder()
+                .loginId(findMember.getLoginId())
+                .email(findMember.getEmail())
+                .accessToken(accessToken)
+                .build();
     }
 
-    public MemberInfoResponse getMemberInfo(String loginId) throws Exception {
+    public MemberInfoResponse getMemberInfo(String loginId) {
         // 멤버 정보 조회
         if(loginId == null) {
-            throw new RuntimeException("잘못된 요청입니다.");
+            throw new IllegalArgumentException("잘못된 요청입니다.");
         }
 
         Optional<Member> optionalMember = memberRepository.findByLoginId(loginId);
         if(optionalMember.isEmpty()) {
-            throw new RuntimeException("정보를 찾을 수 없습니다.");
+            throw new IllegalArgumentException("정보를 찾을 수 없습니다.");
         }
 
         Member findMember = optionalMember.get();
