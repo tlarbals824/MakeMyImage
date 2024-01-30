7091fe691ab401870b075ec27864256c2d79107b
재국 <jk6722@hanyang.ac.kr>
2024-01-30 16:10:45 +0900

    feat: 이미지 조회 메서드 구현

--- src/main/java/com/backend/makemyimage/repository/image/ImageRepository.java
+++ src/main/java/com/backend/makemyimage/repository/image/ImageRepository.java
@@ -3,11 +3,7 @@ package com.backend.makemyimage.repository.image;
 import com.backend.makemyimage.domain.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface ImageRepository extends JpaRepository<Image, Long>, ImageRepositoryCustom {
-    Optional<Image> findById(Long id);
-    List<Image> findAllByLoginId(String loginId);
 }
+
 
