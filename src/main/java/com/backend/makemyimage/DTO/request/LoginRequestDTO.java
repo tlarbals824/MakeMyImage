b79fff9eff84dbe04dce933bf838a48817a20276
j2noo <wlsdndml213@gmail.com>
2024-01-31 20:06:33 +0900

    feat : LoginRequestDTO 생성:

---
+++ src/main/java/com/backend/makemyimage/DTO/request/LoginRequestDTO.java
@@ -0,0 +1,20 @@
+package com.backend.makemyimage.DTO.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter //필요 없나?
+@Setter
+//@Builder
+public class LoginRequestDTO {
+    String email;
+    String password;
+
+    @Builder
+    public LoginRequestDTO(String email, String password) {
+        this.email =email;
+        this.password = password;
+    }
+
+}
