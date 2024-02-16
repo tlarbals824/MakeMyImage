2791e574d62854214880a5d07133ed9def492b07
Bae Hyeonseo <haulqogustj@naver.com>
2024-02-17 02:50:36 +0900

    [feat]: jwt + 예외처리

---
+++ src/main/java/com/backend/makemyimage/global/auth/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.backend.makemyimage.global.auth;
+
+import com.backend.makemyimage.global.exception.errorDto.ErrorCode;
+import com.backend.makemyimage.global.exception.errorDto.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        handleException(response);
+    }
+
+    private void handleException(HttpServletResponse response) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(ErrorCode.UNAUTHORIZED.getStatus().value());
+        response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.of(ErrorCode.UNAUTHORIZED)));
+    }
+}
--- src/main/java/com/backend/makemyimage/global/config/SecurityConfig.java
+++ src/main/java/com/backend/makemyimage/global/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.backend.makemyimage.global.config;
 
+import com.backend.makemyimage.global.auth.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     //spring 2.7  이후에 바뀐 방법, 그냥 requestMatchers에 url넣는거보다 이렇게 사용하는게 더 깔끔해보여서 이런 방법 접하고 이렇게 사용중
     private static final String[] whiteList = {"/"};
     @Bean
@@ -24,6 +32,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagementConfigurer ->
                         sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptionHandlingConfigurer ->
+                        exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .build();
     }
 }
